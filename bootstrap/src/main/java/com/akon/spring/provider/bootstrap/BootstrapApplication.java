36e133dbdf0183a935606adcaa29d1b942e7b47d
akon <chenbin666666@126.com>
2022-06-08 18:44:12 +0800

    aaa

--- bootstrap/src/main/java/com/akon/spring/provider/bootstrap/BootstrapApplication.java
+++ bootstrap/src/main/java/com/akon/spring/provider/bootstrap/BootstrapApplication.java
@@ -1,11 +1,13 @@
 package com.akon.spring.provider.bootstrap;
 
-import com.alibaba.dubbo.config.spring.context.annotation.DubboComponentScan;
+import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = {"com.akon.spring.provider"})
+@EnableDiscoveryClient
 @DubboComponentScan(basePackages = {"com.akon.spring.provider"})
 @MapperScan(basePackages = {"com.akon.spring.provider.repository"})
 public class BootstrapApplication {
